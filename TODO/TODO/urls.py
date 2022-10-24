a037771346429cd0687a75fd3a6b05a2aed8f399
Gimmyhat <nikitchenko.as@gmail.com>
2022-10-24 09:16:41 +0800

    Lesson 10. GraphQL

--- TODO/TODO/urls.py
+++ TODO/TODO/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from graphene_django.views import GraphQLView
 from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from authors.views import AuthorModelViewSet
@@ -57,4 +58,5 @@ urlpatterns = [
                                          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
                                        cache_timeout=0), name='schema-redoc'),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
 ]
